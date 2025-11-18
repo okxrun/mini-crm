35f8b8a8c7aca225434e14fcce93f3b60a4de3c1
okxrun <okxrun@gmail.com>
2025-11-18 13:00:14 +0300

    create registration page

---
+++ registration.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form method="post">
+        <h2 style="margin-bottom:10px;">Sign up</h2>
+        <input type="text" name="name" placeholder="name" required><br><br>
+        <input type="email" name="email" placeholder="email" required><br><br>
+        <input type="password" name="password" placeholder="password" required><br><br>
+        <button type="submit">Sign up</button>
+        <p style="margin-top:5px;">Already have an account? <a href="index.php">Log in<a>.</p>
+    </form>
+</body>
+</html>
