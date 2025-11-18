204b835140e905bd1c46c3d75e7b7bd310345d17
okxrun <okxrun@gmail.com>
2025-11-18 12:50:57 +0300

    login page create

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 require("db.php");
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Authorization</title>
+</head>
+<body>
+    <form method="post">
+        <h2>Login</h2>
+        <input type="email" name="email" placeholder="email"><br><br>
+        <input type="password" name="password" placeholder="password"><br><br>
+        <button type="submit">Login</button>
+        <p>Have no account?<a href="registration.php">Sign up<a>.</p>
+    </form>
+</body>
+</html>
