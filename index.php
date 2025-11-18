40de1205a03d56f12c2fa401d53c5fb0dc3fbe5f
okxrun <okxrun@gmail.com>
2025-11-18 12:57:03 +0300

    styles for login page

--- index.php
+++ index.php
@@ -10,14 +10,15 @@ require("db.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Authorization</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form method="post">
-        <h2>Login</h2>
-        <input type="email" name="email" placeholder="email"><br><br>
-        <input type="password" name="password" placeholder="password"><br><br>
+        <h2 style="margin-bottom:10px;">Login</h2>
+        <input type="email" name="email" placeholder="email" required><br><br>
+        <input type="password" name="password" placeholder="password" required><br><br>
         <button type="submit">Login</button>
-        <p>Have no account?<a href="registration.php">Sign up<a>.</p>
+        <p style="margin-top:5px;">Have no account? <a href="registration.php">Sign up<a>.</p>
     </form>
 </body>
 </html>
